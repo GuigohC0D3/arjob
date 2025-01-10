8c8073178a8e11581dfb0bfed9532b5d8d518f96
GuigohC0D3 <guilhermewerneckpereira11@hotmail.com>
2025-01-10 09:41:56 -0300

    coloquei sessionstorage e fiz autenticação nas rotas.

--- backend/src/utils/hash_utils.py
+++ backend/src/utils/hash_utils.py
@@ -1,12 +1,9 @@
 import bcrypt
 
-def hash_password(plain_password):
-    # Converte a senha para bytes e gera o hash
-    hashed = bcrypt.hashpw(plain_password.encode('utf-8'), bcrypt.gensalt())
-    return hashed.decode('utf-8')  # Retorna o hash como string
+def hash_password(password):
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
 def verify_password(plain_password, hashed_password):
-    # Compara a senha com o hash
     return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
 
 
